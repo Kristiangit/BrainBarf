402da7a3a780f508c78cef3c749f7e13d54a681d
Kristian Numme <k.numme05@gmail.com>
2023-05-24 09:46:54 +0200

    first commit

---
+++ brainbarf/src/app/page.js
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <main className="bg-white">
+      <div className="flex flex-col w-full items-center justify-around text-sm">
+        <div className='bg-green-600'>
+          <span>Navbar</span>
+        </div>
+        <div>
+          <span>Hello World!</span>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </main>
+  )
+}
